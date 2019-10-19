1df58995e3fd4c46f75bd0dbe59d498bb0780aee
Acekith <millergrayson0@gmail.com>
2019-10-18 19:55:58 -0500

    get & set Script basic help made.

---
+++ Get-WindowCoordinates.ps1
@@ -0,0 +1,17 @@
+<#
+.SYNOPSIS
+Gets coordinates for a windowed process
+
+.DESCRIPTION
+Gets the coordinates for a windowed process using the process ID and returns a PSCustomobject containing the
+coordinates of the process window. 
+
+.INPUTS
+ID of process
+
+.OUTPUTS
+PSCustomobject
+
+.NOTES
+v0.1 10/18/2019
+#>
---
+++ Set-WindowCoordinates.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+Sets coordinates for a windowed process
+
+.DESCRIPTION
+Sets the coordinates for a windowed process. requires all 4 coordinates to work. 
+
+.INPUTS
+Coordinates object or coordinates
+
+.OUTPUTS
+Boolean object
+
+.NOTES
+v0.1 10/18/2019
+#>
